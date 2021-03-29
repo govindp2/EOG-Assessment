23a0f02d634dfabe3ab0ba69ad61a0ec83d84211
govindreddypatlolu <32153839+govindreddypatlolu@users.noreply.github.com>
2021-03-29 14:09:56 -0400

    Changes for CardHeader

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -2,11 +2,12 @@ import CardHeader from '@material-ui/core/CardHeader';
 import { withStyles, Theme } from '@material-ui/core/styles';
 
 const styles = (theme: Theme) => ({
-  root: {
-    background: theme.palette.primary.main,
-  },
   title: {
     color: 'white',
   },
+  root: {
+    background: theme.palette.primary.main,
+  },
+  
 });
 export default withStyles(styles)(CardHeader);
